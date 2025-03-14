2ef5d046169e9b4e5167a1ee09d2239cca96ca45
Łukasz Adamczyk <lukas.adamczyk@getvendo.com>
2025-03-14 14:00:54 +0100

    Fix Stripe webhooks, add payment sources serializers

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Spree::Core::Engine.add_routes do
   # Apple Pay domain verification certificate for Apple Pay
   get '/.well-known/apple-developer-merchantid-domain-association' => '/spree_stripe/apple_pay_domain_verification#show'
 
+  # Stripe webhooks
+  mount StripeEvent::Engine, at: '/stripe'
+
   # Storefront API
   namespace :api, defaults: { format: 'json' } do
     namespace :v2 do

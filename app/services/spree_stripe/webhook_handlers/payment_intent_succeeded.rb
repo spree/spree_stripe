ffe2d4fc6e17a3002c123bd6d188ad38a844c5a3
Damian Kaczmarczyk <git@vegann.space>
2025-03-24 12:02:01 +0100

    Use `find_by` instead of `find_by!`

--- app/services/spree_stripe/webhook_handlers/payment_intent_succeeded.rb
+++ app/services/spree_stripe/webhook_handlers/payment_intent_succeeded.rb
@@ -3,7 +3,7 @@ module SpreeStripe
     class PaymentIntentSucceeded
       def call(event)
         payment_intent_data = event.data.object
-        payment_intent = SpreeStripe::PaymentIntent.find_by!(stripe_id: payment_intent_data[:id])
+        payment_intent = SpreeStripe::PaymentIntent.find_by(stripe_id: payment_intent_data[:id])
         return if payment_intent.nil?
 
         SpreeStripe::CompleteOrderJob.set(wait: 10.seconds).perform_later(payment_intent.id)

916c85bd457bdecd6175e1d334245a3b4b6269ae
Damian Kaczmarczyk <git@vegann.space>
2025-04-10 12:17:23 +0200

    Add `publishable_key` to storefront's payment_method serializer

---
+++ app/serializers/spree_stripe/v2/storefront/payment_method_serializer_decorator.rb
@@ -0,0 +1,15 @@
+module SpreeStripe
+  module V2
+    module Storefront
+      module PaymentMethodSerializerDecorator
+        def self.prepended(base)
+          base.attribute :publishable_key do |pm|
+            pm.try(:preferred_publishable_key)
+          end
+        end
+      end
+    end
+  end
+end
+
+Spree::V2::Storefront::PaymentMethodSerializer.prepend(SpreeStripe::V2::Storefront::PaymentMethodSerializerDecorator)
---
+++ spec/serializers/spree_stripe/v2/storefront/payment_method_serializer_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe Spree::V2::Storefront::PaymentMethodSerializer do
+  include_context 'API v2 serializers params'
+
+  subject { described_class.new(resource, params: serializer_params) }
+
+  let!(:store) { Spree::Store.default }
+  let(:resource) { create(:stripe_gateway) }
+
+  it { expect(subject.serializable_hash).to be_kind_of(Hash) }
+
+  it do
+    expect(subject.serializable_hash).to eq(
+      {
+        data: {
+          id: resource.id.to_s,
+          type: :payment_method,
+          attributes: {
+            name: resource.name,
+            description: resource.description,
+            type: resource.type,
+            publishable_key: resource.preferred_publishable_key,
+            preferences: {},
+            public_metadata: {},
+          },
+        }
+      }
+    )
+  end
+end

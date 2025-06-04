e84b00ac19d528d1ebd57d4f1da9aef0083eea51
Damian Legawiec <damian@getvendo.com>
2025-06-04 17:58:13 +0200

    Remove unused factory
    which is also defined in Spree 5.1

--- lib/spree_stripe/testing_support/factories/payment_source_factory.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :payment_source, class: Spree::PaymentSource do
-    association :payment_method, factory: :payment_method
-    gateway_payment_profile_id { 'pm_1adfg34fgfdf5245' }
-    type { 'SpreeStripe::PaymentSources::Alipay' }
-    user
-  end
-end

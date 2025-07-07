b7043558fe4c225651fa6826bb10621b8d116ab4
Łukasz Adamczyk <lukas.adamczyk@getvendo.com>
2025-07-07 18:36:04 +0200

    Fix webhook controller decorator, add specs

--- app/controllers/stripe_event/webhook_controller_decorator.rb
+++ app/controllers/stripe_event/webhook_controller_decorator.rb
@@ -1,5 +1,9 @@
 module StripeEvent
   module WebhookControllerDecorator
+    def self.prepended(base)
+      base.include Spree::Core::ControllerHelpers::Store
+    end
+
     def secrets(payload, signature)
       all_signing_keys = SpreeStripe::WebhookKey.pluck(:signing_secret).uniq.compact
       all_signing_keys << ENV['STRIPE_SIGNING_SECRET'] if ENV['STRIPE_SIGNING_SECRET'].present?

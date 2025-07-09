e2d7d3b8bbdb2e041782ac40184727a9b7f0cc0f
Łukasz Adamczyk <lukas.adamczyk@getvendo.com>
2025-07-09 19:07:09 +0200

    Update the Stripe customer when the user's billing address changes

---
+++ app/models/spree_stripe/address_decorator.rb
@@ -0,0 +1,17 @@
+module SpreeStripe
+  module AddressDecorator
+    def self.prepended(base)
+      base.after_update :update_stripe_customer, if: :should_update_stripe_customer?
+    end
+
+    delegate :update_stripe_customer, to: :user, allow_nil: true
+
+    private
+
+    def should_update_stripe_customer?
+      user_default_billing? && previous_changes.any?
+    end
+  end
+end
+
+Spree::Address.prepend(SpreeStripe::AddressDecorator)

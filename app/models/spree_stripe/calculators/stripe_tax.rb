290f217372c213fe0d43287d13f8ba4b53d16c1f
Damian Legawiec <damian@getvendo.com>
2025-09-04 21:35:41 +0200

    Update stripe_tax.rb

--- app/models/spree_stripe/calculators/stripe_tax.rb
+++ app/models/spree_stripe/calculators/stripe_tax.rb
@@ -52,7 +52,7 @@ module SpreeStripe
       def get_or_create_tax_calculation(order)
         stripe_gateway = order.store.stripe_gateway
 
-        Rails.cache.fetch("stripe_tax_calculation_#{order.item_total}_#{order.shipment_total}_#{order.item_count}", expires_in: 1.hour) do
+        Rails.cache.fetch("stripe_tax_calculation_#{order.total}", expires_in: 1.hour) do
           stripe_gateway.create_tax_calculation(order)
         end
       rescue => e

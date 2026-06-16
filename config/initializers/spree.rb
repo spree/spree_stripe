dddb3a526a482631540b2c0fd5983b1e27d12cc7
Łukasz Adamczyk <lukas.adamczyk@getvendo.com>
2026-06-16 13:32:18 +0200

    LUP-6 Guard against missing legacy API v2

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -4,6 +4,10 @@ Rails.application.config.after_initialize do
 
   if Rails.application.config.respond_to?(:spree_storefront)
     Rails.application.config.spree_storefront.head_partials << 'spree_stripe/head'
-    Rails.application.config.spree_storefront.quick_checkout_partials << 'spree_stripe/quick_checkout'
+
+    # Quick checkout partial depends on the legacy Storefront API v2
+    if defined?(SpreeLegacyApiV2::Engine)
+      Rails.application.config.spree_storefront.quick_checkout_partials << 'spree_stripe/quick_checkout'
+    end
   end
 end
